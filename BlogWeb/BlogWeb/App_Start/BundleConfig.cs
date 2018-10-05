c7fbbf6633e5327a9a09616930455e0e851c20f3
ES94 <e.stradella94@outlook.com>
2018-10-05 04:20:53 -0300

    Commit N° 3

--- BlogWeb/BlogWeb/App_Start/BundleConfig.cs
+++ BlogWeb/BlogWeb/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace BlogWeb
 {
@@ -14,6 +13,12 @@ namespace BlogWeb
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+               bundles.Add(new StyleBundle("~/bundles/css")
+                .Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/fontawesome-free/css/all.min.css",
+                "~/Content/css/clean-blog.min.css"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +30,12 @@ namespace BlogWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+              bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/js/clean-blog.min.js"));
         }
     }
 }
